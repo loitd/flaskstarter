49d38dd6cab95bc11d88889f9a60ae2121812183
Tran Duc Loi <loitranduc@gmail.com>
2018-10-26 13:32:46 +0700

    initial4

--- app/instance/config.py
+++
@@ -1,5 +0,0 @@
-# All configuration keys for this instance
-SECRET_KEY = '75-eewfaibfwo4657890paacsO(*&^*INkf0wfisaifsafa'
-DEBUG = True
-ENV = 'development'
-SQLALCHEMY_DATABASE_URI = "oracle+cx_oracle://VIRTUAL_ACCOUNT:VIRTUAL_ACCOUNT123@172.16.10.84/DB84"
